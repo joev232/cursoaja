050636b8a79cdec7abb1ac9ec8abfd81b3a18700
joev232 <joev232@hotmail.com>
2015-07-23 20:52:31 +0200

    d

---
+++ Curso7/src/com/ejemplo/servlet/ServletJuego.java
@@ -0,0 +1,15 @@
+package com.ejemplo.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class ServletJuego extends HttpServlet {
+
+	//atributo clases
+	String array_frases[]= {"Felicidades","Muy Bien","Ganador"};
+	
+	//en init inicializarlo
+	
+	
+	
+	
+}
